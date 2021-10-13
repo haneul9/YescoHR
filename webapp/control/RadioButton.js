155c3dd2f3d9bae954d5c6e3dfabb111059c1d66
성환희 <haneul9@gmail.com>
2021-10-13 09:02:01 +0900

    #Pub-컴포넌트 스타일

---
+++ webapp/control/RadioButton.js
@@ -0,0 +1,35 @@
+sap.ui.define(
+  [
+    'sap/m/RadioButton', //
+  ],
+  function (RadioButton) {
+    'use strict';
+
+    return RadioButton.extend('sap.ui.yesco.control.RadioButton', {
+      metadata: {
+        properties: {
+          outerSize: {
+            type: 'sap.ui.core.CSSSize',
+            defaultValue: '50%',
+          },
+          innerSize: {
+            type: 'sap.ui.core.CSSSize',
+            defaultValue: '22%',
+          },
+        },
+      },
+      onAfterRendering: function () {
+        if (sap.ui.core.Control.prototype.onAfterRendering) {
+          sap.ui.core.Control.prototype.onAfterRendering.apply(this, arguments); //run the super class's method first
+        }
+
+        const sId = this.getId();
+        const aCircleNodes = document.getElementById(sId).childNodes[0].childNodes[0].childNodes;
+
+        aCircleNodes[0].setAttribute('r', this.getOuterSize());
+        aCircleNodes[1].setAttribute('r', this.getInnerSize());
+      },
+      renderer: {},
+    });
+  }
+);
