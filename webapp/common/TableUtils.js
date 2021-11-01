d90e4242164e9821fc22e0c6301af9950396ee9e
성환희 <haneul9@gmail.com>
2021-11-01 14:33:30 +0900

    테이블 공통화

---
+++ webapp/common/TableUtils.js
@@ -0,0 +1,123 @@
+sap.ui.define(
+  [
+    'sap/ui/export/Spreadsheet', //
+    'sap/ui/yesco/extension/moment',
+    'sap/ui/yesco/extension/lodash',
+  ],
+  (
+    Spreadsheet //
+  ) => {
+    'use strict';
+
+    /**
+     * @constant {number} 임시저장
+     */
+    const STATE_IN_PROGRESS1 = 10;
+    /**
+     * @constant {number} 결재취소
+     */
+    const STATE_IN_PROGRESS2 = 90;
+    /**
+     * @constant {number} 신청완료
+     */
+    const STATE_APPLY1 = 20;
+    /**
+     * @constant {number} 담당자 접수
+     */
+    const STATE_APPLY2 = 30;
+    /**
+     * @constant {number} 결재 기안
+     */
+    const STATE_APPLY3 = 50;
+    /**
+     * @constant {number} 담당자 승인
+     */
+    const STATE_APPROVE = 40;
+    /**
+     * @constant {number} 담당자 반려
+     */
+    const STATE_REJECT1 = 45;
+    /**
+     * @constant {number} 결재 반려
+     */
+    const STATE_REJECT2 = 65;
+    /**
+     * @constant {number} 결재 승인
+     */
+    const STATE_COMPLETE = 60;
+
+    return {
+      /**************************
+       * Formatter
+       *************************/
+      rowHighlight(sValue) {
+        switch (parseInt(sValue, 10)) {
+          case STATE_IN_PROGRESS1:
+          case STATE_IN_PROGRESS2:
+            return sap.ui.core.IndicationColor.Indication01;
+          case STATE_APPLY1:
+          case STATE_APPLY2:
+          case STATE_APPLY3:
+            return sap.ui.core.IndicationColor.Indication03;
+          case STATE_APPROVE:
+            return sap.ui.core.IndicationColor.Indication04;
+          case STATE_REJECT1:
+          case STATE_REJECT2:
+            return sap.ui.core.IndicationColor.Indication02;
+          case STATE_COMPLETE:
+            return sap.ui.core.IndicationColor.Indication05;
+          default:
+            return null;
+        }
+      },
+
+      /**************************
+       * Functions
+       *************************/
+      count({ mTableData }) {
+        const oViewModel = this.getViewModel();
+        const aZappStatAls = mTableData.map((obj) => obj.ZappStatAl);
+        const oOccurCount = _.defaults(_.countBy(aZappStatAls), {
+          [STATE_IN_PROGRESS1]: 0,
+          [STATE_IN_PROGRESS2]: 0,
+          [STATE_APPLY1]: 0,
+          [STATE_APPLY2]: 0,
+          [STATE_APPLY3]: 0,
+          [STATE_APPROVE]: 0,
+          [STATE_REJECT1]: 0,
+          [STATE_REJECT2]: 0,
+          [STATE_COMPLETE]: 0,
+        });
+
+        oViewModel.setProperty('/listinfo', {
+          rowCount: aZappStatAls.length > 10 ? 10 : aZappStatAls.length || 1,
+          totalCount: aZappStatAls.length,
+          progressCount: oOccurCount[STATE_IN_PROGRESS1] + oOccurCount[STATE_IN_PROGRESS2],
+          applyCount: oOccurCount[STATE_APPLY1] + oOccurCount[STATE_APPLY2] + oOccurCount[STATE_APPLY3],
+          approveCount: oOccurCount[STATE_APPROVE],
+          rejectCount: oOccurCount[STATE_REJECT1] + oOccurCount[STATE_REJECT2],
+          completeCount: oOccurCount[STATE_COMPLETE],
+        });
+      },
+
+      export({ aColumns, mTableData, sFileName }) {
+        const sToday = moment().format('YYYYMMDD');
+
+        const oSettings = {
+          workbook: {
+            columns: aColumns,
+            hierarchyLevel: 'Level',
+          },
+          dataSource: mTableData,
+          fileName: `${sFileName}_${sToday}.xlsx`,
+          worker: false, // We need to disable worker because we are using a MockServer as OData Service
+        };
+
+        const oSheet = new Spreadsheet(oSettings);
+        oSheet.build().finally(function () {
+          oSheet.destroy();
+        });
+      },
+    };
+  }
+);
--- webapp/controller/attendance/List.controller.js
+++ webapp/controller/attendance/List.controller.js
@@ -4,9 +4,10 @@ sap.ui.define(
     'sap/ui/model/Filter',
     'sap/ui/model/FilterOperator',
     'sap/ui/model/json/JSONModel',
+    'sap/ui/export/library',
     'sap/ui/yesco/controller/BaseController',
     'sap/ui/yesco/common/EmpInfo',
-    '../../model/formatter',
+    'sap/ui/yesco/common/TableUtils',
     'sap/ui/yesco/extension/moment',
   ],
   (
@@ -14,20 +15,22 @@ sap.ui.define(
     Filter,
     FilterOperator,
     JSONModel,
+    exportLibrary,
     BaseController,
     EmpInfo,
-    formatter
+    TableUtils
   ) => {
     'use strict';
 
     class List extends BaseController {
       constructor() {
         super();
-        this.formatter = formatter;
+        this.formatter = TableUtils;
       }
 
       onInit() {
         const oViewModel = new JSONModel({
+          busy: false,
           search: {
             Apbeg: moment().subtract(1, 'month').startOf('month').hours(9).toDate(),
             Apend: moment().hours(9).toDate(),
@@ -57,22 +60,54 @@ sap.ui.define(
         const oModel = this.getModel('worktime');
         const oViewModel = this.getViewModel();
         const oSearchConditions = oViewModel.getProperty('/search');
+        const sUrl = '/LeaveApplContentSet';
 
-        oModel.read('/LeaveApplContentSet', {
+        oViewModel.setProperty('/busy', true);
+
+        oModel.read(sUrl, {
           filters: [
-            new Filter('Apbeg', FilterOperator.EQ, oSearchConditions.Apbeg), //
-            new Filter('Apend', FilterOperator.EQ, oSearchConditions.Apend),
+            new Filter('Apbeg', FilterOperator.EQ, moment(oSearchConditions.Apbeg).hours(9).toDate()), //
+            new Filter('Apend', FilterOperator.EQ, moment(oSearchConditions.Apend).hours(9).toDate()),
           ],
           success: (oData) => {
-            this.debug('success.', oData);
             oViewModel.setProperty('/list', oData.results);
-            oViewModel.setProperty('/listinfo/rowCount', oData.results.length > 10 ? 10 : oData.results.length || 1);
-            oViewModel.setProperty('/listinfo/totalCount', oData.results.length);
+            TableUtils.count.call(this, { mTableData: oData.results });
+
+            oViewModel.setProperty('/busy', false);
           },
           error: (oError) => {
             this.debug(`${sUrl} error.`, oError);
+
+            oViewModel.setProperty('/busy', false);
           },
         });
+      }
+
+      createColumnConfig() {
+        const aCols = [
+          { label: '상태', property: 'ZappStxtAl', type: exportLibrary.EdmType.String },
+          { label: '구분', property: 'Dettx', type: exportLibrary.EdmType.String },
+          { label: '사번', property: 'Appernr', type: exportLibrary.EdmType.String },
+          { label: '성명', property: 'Apename', type: exportLibrary.EdmType.String },
+          { label: '부서', property: 'Aporgtx', type: exportLibrary.EdmType.String },
+          { label: '직급', property: 'Apzzjikgbtx', type: exportLibrary.EdmType.String },
+          { label: '근태', property: 'Atext', type: exportLibrary.EdmType.String },
+          { label: '시작일', property: 'Apbeg', type: exportLibrary.EdmType.Date },
+          { label: '종료일', property: 'Apend', type: exportLibrary.EdmType.Date },
+          { label: '기간', property: 'Period', type: exportLibrary.EdmType.String },
+          { label: '신청일', property: 'Appdt', type: exportLibrary.EdmType.Date },
+          { label: '결재일', property: 'Sgndt', type: exportLibrary.EdmType.Date },
+        ];
+
+        return aCols;
+      }
+
+      onExelDownload() {
+        const aColumns = this.createColumnConfig();
+        const mTableData = this.getViewModel().getProperty('/list');
+        const sFileName = '근태신청_목록';
+
+        TableUtils.export({ aColumns, mTableData, sFileName });
       }
     }
 
