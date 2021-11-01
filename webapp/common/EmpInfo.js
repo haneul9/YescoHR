2c4d77bb6007c051c64d2fe088ab4abb6ff98bc6
성환희 <haneul9@gmail.com>
2021-11-01 10:39:28 +0900

    근태신청 목록 수정

--- webapp/common/EmpInfo.js
+++ webapp/common/EmpInfo.js
@@ -1,27 +1,27 @@
 sap.ui.define([], () => {
-    'use strict';
-  
-    return {
-        getInfo(Hide) {
-            const oCommonModel = this.getModel();
-            const sUrl = '/EmpLoginInfoSet';
-            const oViewModel = this.getViewModel();
-  
-            oCommonModel.read(sUrl, {
-              success: (oData, oResponse) => {
-                this.debug(`${sUrl} success.`, oData, oResponse);
-                const oLoginInfo = oData.results[0];
-  
-                if(Hide === "o")
-                    oLoginInfo.Hide = "o";
+  'use strict';
 
-                oViewModel.setData({TargetInfo: oLoginInfo});
-              },
-              error: (oError) => {
-                this.debug(`${sUrl} error.`, oError);
-              },
-            });
-          }
-    };
-  });
-  
+  return {
+    getInfo(Hide) {
+      const oCommonModel = this.getModel();
+      const sUrl = '/EmpLoginInfoSet';
+      const oViewModel = this.getViewModel();
+      const oViewModelData = this.getViewModel().getData();
+
+      oCommonModel.read(sUrl, {
+        success: (oData, oResponse) => {
+          this.debug(`${sUrl} success.`, oData, oResponse);
+          const oLoginInfo = oData.results[0];
+
+          if (Hide === 'o') oLoginInfo.Hide = 'o';
+
+          oViewModelData.TargetInfo = oLoginInfo || {};
+          oViewModel.setData(oViewModelData);
+        },
+        error: (oError) => {
+          this.debug(`${sUrl} error.`, oError);
+        },
+      });
+    },
+  };
+});
--- webapp/controller/attendance/List.controller.js
+++ webapp/controller/attendance/List.controller.js
@@ -5,7 +5,9 @@ sap.ui.define(
     'sap/ui/model/FilterOperator',
     'sap/ui/model/json/JSONModel',
     'sap/ui/yesco/controller/BaseController',
+    'sap/ui/yesco/common/EmpInfo',
     '../../model/formatter',
+    'sap/ui/yesco/extension/moment',
   ],
   (
     // prettier 방지용 주석
@@ -13,6 +15,7 @@ sap.ui.define(
     FilterOperator,
     JSONModel,
     BaseController,
+    EmpInfo,
     formatter
   ) => {
     'use strict';
@@ -20,14 +23,56 @@ sap.ui.define(
     class List extends BaseController {
       constructor() {
         super();
-
         this.formatter = formatter;
       }
 
       onInit() {
-        const oViewModel = new JSONModel();
-        oViewModel.loadData('localService/attendancedata.json');
+        const oViewModel = new JSONModel({
+          search: {
+            Apbeg: moment().subtract(1, 'month').startOf('month').hours(9).toDate(),
+            Apend: moment().hours(9).toDate(),
+          },
+          listinfo: {
+            rowCount: 1,
+            totalCount: 0,
+            progressCount: 0,
+            applyCount: 0,
+            approveCount: 0,
+            rejectCount: 0,
+            completeCount: 0,
+          },
+          list: [],
+        });
+        // oViewModel.loadData('localService/attendancedata.json');
         this.setViewModel(oViewModel);
+
+        // 대상자 정보
+        EmpInfo.getInfo.call(this, 'o');
+
+        // 목록 조회
+        this.search();
+      }
+
+      search() {
+        const oModel = this.getModel('worktime');
+        const oViewModel = this.getViewModel();
+        const oSearchConditions = oViewModel.getProperty('/search');
+
+        oModel.read('/LeaveApplContentSet', {
+          filters: [
+            new Filter('Apbeg', FilterOperator.EQ, oSearchConditions.Apbeg), //
+            new Filter('Apend', FilterOperator.EQ, oSearchConditions.Apend),
+          ],
+          success: (oData) => {
+            this.debug('success.', oData);
+            oViewModel.setProperty('/list', oData.results);
+            oViewModel.setProperty('/listinfo/rowCount', oData.results.length > 10 ? 10 : oData.results.length || 1);
+            oViewModel.setProperty('/listinfo/totalCount', oData.results.length);
+          },
+          error: (oError) => {
+            this.debug(`${sUrl} error.`, oError);
+          },
+        });
       }
     }
 
