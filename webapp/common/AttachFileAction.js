e9a6dbece0be2d2224df5c18a18ab41d08fcc12e
JHU <HPJT0897@lottechem.com>
2021-11-12 09:39:54 +0900

    Fix 첨부파일 수정

--- webapp/common/AttachFileAction.js
+++ webapp/common/AttachFileAction.js
@@ -38,7 +38,7 @@ sap.ui.define(
                 oFileUploader.setValue("");
 
                 options.ListMode = options.Editable ? sap.ui.table.SelectionMode.MultiToggle : sap.ui.table.SelectionMode.None;
-                options.FileTypes = ["ppt", "pptx", "doc", "docx", "xls", "xlsx", "jpg", "bmp", "gif", "png", "txt", "pdf", "jpeg"];
+                options.FileTypes = !!opt.FileTypes ? opt.FileTypes : ["ppt", "pptx", "doc", "docx", "xls", "xlsx", "jpg", "bmp", "gif", "png", "txt", "pdf", "jpeg"];
 
                 oController.getViewModel().setProperty("/Settings", options);
                 oController.getViewModel().setProperty("/DelelteDatas", []);
