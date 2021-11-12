4baeec06fb77ff40afccb7d4d87a17294fe13c10
유정우 <enmoei@gmail.com>
2021-11-12 18:03:52 +0900

    DatePicker, DateRangeSelection "오늘" 버튼 적용 샘플 추가

---
+++ webapp/control/DateRangeSelection.js
@@ -0,0 +1,43 @@
+sap.ui.define(
+  [
+    // prettier 방지용 주석
+    'sap/m/DateRangeSelection',
+    'sap/ui/yesco/common/AppUtils',
+  ],
+  function (
+    // prettier 방지용 주석
+    sapMDateRangeSelection,
+    AppUtils
+  ) {
+    'use strict';
+
+    class DateRangeSelection extends sapMDateRangeSelection {
+      _createPopup() {
+        super._createPopup();
+
+        this._oPopup.getBeginButton().setText(AppUtils.getAppController().getText('LABEL_01005')); // 오늘
+
+        const endButton = this._oPopup.getEndButton();
+        if (endButton) {
+          endButton.setVisible(false);
+        }
+      }
+
+      _createPopupContent() {
+        super._createPopupContent();
+
+        this._oPopup.getBeginButton().setEnabled(true);
+      }
+
+      _handleOKButton() {
+        this._oCalendar.focusDate(new Date());
+      }
+
+      _handleCalendarSelect() {
+        this._selectDate();
+      }
+    }
+
+    return DateRangeSelection;
+  }
+);
---
+++ webapp/controller/zample/DatePicker.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define(
+  [
+    // prettier 방지용 주석
+    'sap/ui/yesco/controller/BaseController',
+  ],
+  function (
+    // prettier 방지용 주석
+    BaseController
+  ) {
+    'use strict';
+
+    class DatePicker extends BaseController {}
+
+    return DatePicker;
+  }
+);
