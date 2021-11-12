8f430491be1f3d80101b968545b2daf23028ff96
유정우 <enmoei@gmail.com>
2021-11-12 11:12:46 +0900

    MessageBox alert 타이틀 안내로 변경

--- webapp/control/MessageBox.js
+++ webapp/control/MessageBox.js
@@ -14,25 +14,25 @@ sap.ui.define(
     return {
       ...SapMMessageBox,
       alert(vMessage, mOptions = {}) {
-        SapMMessageBox.alert(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_ALERT') });
+        SapMMessageBox.alert(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_ALERT') }); // 안내
       },
       confirm(vMessage, mOptions = {}) {
-        SapMMessageBox.confirm(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_CONFIRM') });
+        SapMMessageBox.confirm(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_CONFIRM') }); // 확인
       },
       error(vMessage, mOptions = {}) {
-        SapMMessageBox.error(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_ERROR') });
+        SapMMessageBox.error(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_ERROR') }); // 오류
       },
       information(vMessage, mOptions = {}) {
-        SapMMessageBox.information(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_INFORMATION') });
+        SapMMessageBox.information(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_INFORMATION') }); // 정보
       },
       show(vMessage, mOptions = {}) {
-        SapMMessageBox.show(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_SHOW') });
+        SapMMessageBox.show(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_SHOW') }); // 보기
       },
       success(vMessage, mOptions = {}) {
-        SapMMessageBox.success(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_SUCCESS') });
+        SapMMessageBox.success(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_SUCCESS') }); // 성공
       },
       warning(vMessage, mOptions = {}) {
-        SapMMessageBox.warning(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_WARNING') });
+        SapMMessageBox.warning(vMessage, { ...mOptions, title: AppUtils.getAppController().getText('MSG_WARNING') }); // 경고
       },
     };
   }
