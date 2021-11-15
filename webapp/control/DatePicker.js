363ec8ac850daf5311b29ec3eb4ea0a6f2e90e8a
유정우 <enmoei@gmail.com>
2021-11-15 10:50:41 +0900

    DatePicker, DateRangeSelection "오늘" 버튼 적용 샘플 수정

--- webapp/control/DatePicker.js
+++ webapp/control/DatePicker.js
@@ -2,34 +2,43 @@ sap.ui.define(
   [
     // prettier 방지용 주석
     'sap/m/DatePicker',
+    'sap/m/ButtonType',
     'sap/ui/yesco/common/AppUtils',
   ],
   function (
     // prettier 방지용 주석
     sapMDatePicker,
+    ButtonType,
     AppUtils
   ) {
-    'use strict';
+    ('use strict');
 
+    /**
+     * '오늘' 버튼 기능을 구현한 DatePicker
+     * ResponsivePopover Footer의 '확인' 버튼을 감추고 '취소' 버튼을 '오늘' 버튼으로 오버라이딩
+     */
     class DatePicker extends sapMDatePicker {
       _createPopup() {
         super._createPopup();
 
-        this._oPopup.getBeginButton().setText(AppUtils.getAppController().getText('LABEL_01005')); // 오늘
+        this._oPopup.getBeginButton().setVisible(false);
 
-        const endButton = this._oPopup.getEndButton();
-        if (endButton) {
-          endButton.setVisible(false);
+        const sTodayText = AppUtils.getAppController().getText('LABEL_01005'); // 오늘
+        const oEndButton = this._oPopup.getEndButton();
+        if (oEndButton) {
+          oEndButton.setType(ButtonType.Emphasized).setText(sTodayText);
+        } else {
+          this._oPopup.setEndButton(
+            new Button({
+              text: sTodayText,
+              type: ButtonType.Emphasized,
+              press: this._handleCancelButton.bind(this),
+            })
+          );
         }
       }
 
-      _createPopupContent() {
-        super._createPopupContent();
-
-        this._oPopup.getBeginButton().setEnabled(true);
-      }
-
-      _handleOKButton() {
+      _handleCancelButton() {
         this._oCalendar.focusDate(new Date());
       }
 
