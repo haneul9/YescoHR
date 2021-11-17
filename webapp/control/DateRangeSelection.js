2ca3b6d5000db8f8acf0ac06b77e00896c887642
유정우 <enmoei@gmail.com>
2021-11-17 17:20:47 +0900

    공통 Custom DatePicker 수정

--- webapp/control/DateRangeSelection.js
+++ webapp/control/DateRangeSelection.js
@@ -18,12 +18,20 @@ sap.ui.define(
      * ResponsivePopover Footer의 '확인' 버튼을 감추고 '취소' 버튼을 '오늘' 버튼으로 오버라이딩
      */
     class DateRangeSelection extends sapMDateRangeSelection {
+      constructor() {
+        super();
+
+        this.setShowFooter(true);
+        this.setPlaceholder('yyyy.MM.dd - yyyy.MM.dd');
+        this.setDisplayFormat('yyyy.MM.dd');
+      }
+
       _createPopup() {
         super._createPopup();
 
         this._oPopup.getBeginButton().setVisible(false);
 
-        const sTodayText = AppUtils.getAppController().getText('LABEL_01005'); // 오늘
+        const sTodayText = AppUtils.getBundleText('LABEL_01005'); // 오늘
         const oEndButton = this._oPopup.getEndButton();
         if (oEndButton) {
           oEndButton.setType(ButtonType.Emphasized).setText(sTodayText);
