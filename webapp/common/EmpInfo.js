3138242386319292672e69f4dfef219e56e251c1
성환희 <haneul9@gmail.com>
2021-11-17 13:37:46 +0900

    get session 변경

--- webapp/common/EmpInfo.js
+++ webapp/common/EmpInfo.js
@@ -9,13 +9,13 @@ sap.ui.define(
 
       return {
         get(bTargetChangeButtonHide = false) {
-          const oSessionData = this.getModel('sessionModel').getData();
+          const oSessionData = this.getOwnerComponent().getSessionModel().getData();
           const oViewModel = this.getViewModel();
           const oViewModelData = this.getViewModel().getData();
-          
+
           // 사원이미지가 없을때 기본이미지
-          if(!oSessionData.Photo) {
-            oSessionData.Photo = "https://i1.wp.com/jejuhydrofarms.com/wp-content/uploads/2020/05/blank-profile-picture-973460_1280.png?ssl=1"
+          if (!oSessionData.Photo) {
+            oSessionData.Photo = 'https://i1.wp.com/jejuhydrofarms.com/wp-content/uploads/2020/05/blank-profile-picture-973460_1280.png?ssl=1';
           }
 
           oViewModelData.TargetInfo = {
