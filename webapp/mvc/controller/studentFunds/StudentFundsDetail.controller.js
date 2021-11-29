8025029eb0bd9f0476a41acdc597d11d1b43422d
JHU <HPJT0897@lottechem.com>
2021-11-29 18:49:27 +0900

    주택융자및 공통 Date type 수정

--- webapp/mvc/controller/studentFunds/StudentFundsDetail.controller.js
+++ webapp/mvc/controller/studentFunds/StudentFundsDetail.controller.js
@@ -12,6 +12,7 @@ sap.ui.define(
     'sap/ui/yesco/common/odata/ServiceNames',
     'sap/ui/yesco/control/MessageBox',
     'sap/ui/yesco/mvc/controller/BaseController',
+    'sap/ui/yesco/mvc/model/ODataDate', // DatePicker 에러 방지 import : Loading of data failed: Error: Date must be a JavaScript date object
   ],
   (
     // prettier 방지용 주석
