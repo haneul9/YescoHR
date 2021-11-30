433eae2cbde1344bad5fb01651f66e86722dbb26
성환희 <haneul9@gmail.com>
2021-11-30 09:40:58 +0900

    화상조직도 샘플 추가

---
+++ webapp/control/D3OrgChartItem.js
@@ -0,0 +1,30 @@
+sap.ui.define(
+  [
+    // prettier 방지용 주석
+    'sap/ui/core/Element',
+  ],
+  function (
+    // prettier 방지용 주석
+    Element
+  ) {
+    'use strict';
+
+    return Element.extend('sap.ui.yesco.control.D3OrgChartItem', {
+      metadata: {
+        properties: {
+          nodeId: { type: 'String', defaultValue: null },
+          parentNodeId: { type: 'String', defaultValue: null },
+          Stdat: { type: 'Object', defaultValue: null },
+          Stext: { type: 'String', defaultValue: null },
+          Pernr: { type: 'String', defaultValue: null },
+          Ename: { type: 'String', defaultValue: null },
+          Ipdat: { type: 'Object', defaultValue: null },
+          Photo: { type: 'String', defaultValue: null },
+          Botxt: { type: 'String', defaultValue: null },
+          Jikgbtl: { type: 'String', defaultValue: null },
+          ZorgLevl: { type: 'String', defaultValue: null },
+        },
+      },
+    });
+  }
+);
