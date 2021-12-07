7a45f52fa131f699d0239c53659f414563a7c480
성환희 <haneul9@gmail.com>
2021-12-07 17:50:58 +0900

    성과평가

---
+++ webapp/mvc/controller/performance/Detail.controller.js
@@ -0,0 +1,92 @@
+sap.ui.define(
+  [
+    // prettier 방지용 주석
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/yesco/common/AppUtils',
+    'sap/ui/yesco/common/exceptions/ODataReadError',
+    'sap/ui/yesco/common/odata/ServiceNames',
+    'sap/ui/yesco/common/TableUtils',
+    'sap/ui/yesco/mvc/controller/BaseController',
+    'sap/ui/yesco/mvc/model/type/Date', // DatePicker 에러 방지 import : Loading of data failed: Error: Date must be a JavaScript date object
+    'sap/ui/yesco/mvc/model/type/Pernr',
+  ],
+  (
+    // prettier 방지용 주석
+    Filter,
+    FilterOperator,
+    JSONModel,
+    AppUtils,
+    ODataReadError,
+    ServiceNames,
+    TableUtils,
+    BaseController
+  ) => {
+    'use strict';
+
+    return BaseController.extend('sap.ui.yesco.mvc.controller.performance.Detail', {
+      onBeforeShow() {
+        const oViewModel = new JSONModel({
+          busy: false,
+          year: 2021,
+          tab: { selectedKey: 'T01' },
+        });
+        this.setViewModel(oViewModel);
+      },
+
+      async onObjectMatched() {
+        const oModel = this.getModel(ServiceNames.APPRAISAL);
+        const oViewModel = this.getViewModel();
+
+        // try {
+        //   oViewModel.setProperty('/busy', true);
+
+        //   const aTableData = await this.readAppraisalPeeList({ oModel });
+
+        //   this.setTableData({ oViewModel, aTableData });
+        // } catch (oError) {
+        //   this.debug('Controller > Performance List > onObjectMatched Error', oError);
+
+        //   AppUtils.handleError(oError);
+        // } finally {
+        //   oViewModel.setProperty('/busy', false);
+        // }
+      },
+
+      /*****************************************************************
+       * ! Event handler
+       *****************************************************************/
+
+      /*****************************************************************
+       * ! Call oData
+       *****************************************************************/
+      /**
+       * @param  {JSONModel} oModel
+       */
+      readAppraisalPeeList({ oModel }) {
+        const mAppointee = this.getAppointeeData();
+        const sUrl = '/AppraisalPeeListSet';
+
+        return new Promise((resolve, reject) => {
+          oModel.read(sUrl, {
+            filters: [
+              new Filter('Werks', FilterOperator.EQ, mAppointee.Werks), //
+              new Filter('Zzappee', FilterOperator.EQ, mAppointee.Pernr),
+            ],
+            success: (oData) => {
+              this.debug(`${sUrl} success.`, oData);
+
+              resolve(oData.results ?? []);
+            },
+            error: (oError) => {
+              this.debug(`${sUrl} error.`, oError);
+
+              reject(new ODataReadError(oError));
+            },
+          });
+        });
+      },
+    });
+  }
+);
