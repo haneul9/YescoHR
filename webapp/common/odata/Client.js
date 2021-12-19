1878947d4c7743d3e818cad707b15f6625c1b57c
성환희 <haneul9@gmail.com>
2021-12-19 20:18:36 +0900

    Client import

--- webapp/common/odata/Client.js
+++ webapp/common/odata/Client.js
@@ -4,12 +4,18 @@ sap.ui.define(
     'sap/ui/model/Filter',
     'sap/ui/model/FilterOperator',
     'sap/ui/yesco/common/AppUtils',
+    'sap/ui/yesco/common/exceptions/ODataReadError',
+    'sap/ui/yesco/common/exceptions/ODataCreateError',
+    'sap/ui/yesco/common/exceptions/ODataDeleteError',
   ],
   (
     // prettier 방지용 주석
     Filter,
     FilterOperator,
-    AppUtils
+    AppUtils,
+    ODataReadError,
+    ODataCreateError,
+    ODataDeleteError
   ) => {
     'use strict';
 
@@ -19,12 +25,12 @@ sap.ui.define(
           oModel.read(`/${sUrl}Set`, {
             filters: _.map(mFilters, (v, p) => new Filter(p, FilterOperator.EQ, v)),
             success: (oData) => {
-              AppUtils.debug(`${sUrl} success.`, oData);
+              AppUtils.debug(`${sUrl} get-entityset success.`, oData);
 
               resolve(oData.results ?? []);
             },
             error: (oError) => {
-              AppUtils.debug(`${sUrl} error.`, oError);
+              AppUtils.debug(`${sUrl} get-entityset error.`, oError);
 
               reject(new ODataReadError(oError));
             },
@@ -36,12 +42,12 @@ sap.ui.define(
         return new Promise((resolve, reject) => {
           oModel.read(oModel.createKey(`/${sUrl}Set`, mKeyMap), {
             success: (oData) => {
-              AppUtils.debug(`${sUrl} success.`, oData);
+              AppUtils.debug(`${sUrl} get success.`, oData);
 
               resolve(oData ?? {});
             },
             error: (oError) => {
-              AppUtils.debug(`${sUrl} error.`, oError);
+              AppUtils.debug(`${sUrl} get error.`, oError);
 
               reject(new ODataReadError(oError));
             },
@@ -53,14 +59,14 @@ sap.ui.define(
         return new Promise((resolve, reject) => {
           oModel.create(`/${sUrl}Set`, mPayload, {
             success: (oData) => {
-              AppUtils.debug(`${sUrl} success.`, oData);
+              AppUtils.debug(`${sUrl} create success.`, oData);
 
               resolve(oData ?? {});
             },
             error: (oError) => {
-              AppUtils.debug(`${sUrl} error.`, oError);
+              AppUtils.debug(`${sUrl} create error.`, oError);
 
-              reject(new ODataReadError(oError));
+              reject(new ODataCreateError(oError));
             },
           });
         });
@@ -70,14 +76,31 @@ sap.ui.define(
         return new Promise((resolve, reject) => {
           oModel.create(`/${sUrl}Set`, mPayload, {
             success: (oData) => {
-              AppUtils.debug(`${sUrl} success.`, oData);
+              AppUtils.debug(`${sUrl} deep success.`, oData);
 
               resolve(oData ?? {});
             },
             error: (oError) => {
-              AppUtils.debug(`${sUrl} error.`, oError);
+              AppUtils.debug(`${sUrl} deep error.`, oError);
 
               reject(new ODataReadError(oError));
+            },
+          });
+        });
+      }),
+
+      remove: _.curry((oModel, sUrl, mKeyMap = {}) => {
+        return new Promise((resolve, reject) => {
+          oModel.remove(oModel.createKey(`/${sUrl}Set`, mKeyMap), {
+            success: (oData) => {
+              AppUtils.debug(`${sUrl} remove success.`, oData);
+
+              resolve(oData ?? {});
+            },
+            error: (oError) => {
+              AppUtils.debug(`${sUrl} remove error.`, oError);
+
+              reject(new ODataDeleteError(oError));
             },
           });
         });
