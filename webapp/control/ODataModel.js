f3403018a9f74e5da4719531cda25974a0383697
성환희 <haneul9@gmail.com>
2021-12-24 14:44:08 +0900

    create 호출시 date, number 변환

--- webapp/control/ODataModel.js
+++ webapp/control/ODataModel.js
@@ -3,11 +3,13 @@ sap.ui.define(
     // prettier 방지용 주석
     'sap/ui/model/odata/v2/ODataModel',
     'sap/ui/yesco/common/AppUtils',
+    'sap/ui/yesco/common/DateUtils',
   ],
   function (
     // prettier 방지용 주석
     ODataModel,
-    AppUtils
+    AppUtils,
+    DateUtils
   ) {
     ('use strict');
 
@@ -22,7 +24,8 @@ sap.ui.define(
           const mEntityMetadata = mModelMetadata[sEntityKey];
 
           Object.keys(oData).forEach((key) => {
-            if (_.isEqual(oData[key], 'ALL')) _.set(oData, key, _.noop());
+            this.convertValue(oData, key);
+
             if (!_.has(mEntityMetadata, key) && !Array.isArray(oData[key])) {
               delete oData[key];
             } else if (Array.isArray(oData[key])) {
@@ -30,7 +33,7 @@ sap.ui.define(
 
               oData[key].forEach((ass) => {
                 Object.keys(ass).forEach((assKey) => {
-                  if (_.isEqual(ass[assKey], 'ALL')) _.set(ass, assKey, _.noop());
+                  this.convertValue(ass, assKey);
                   if (!_.has(mAssociationMetadata, assKey)) delete ass[assKey];
                 });
               });
@@ -40,6 +43,18 @@ sap.ui.define(
 
         if (ODataModel.prototype.create) {
           ODataModel.prototype.create.apply(this, arguments); //run the super class's method first
+        }
+      },
+
+      convertValue(mData, sKey) {
+        const sValue = mData[sKey];
+
+        if (_.isEqual(sValue, 'ALL')) {
+          _.set(mData, sKey, _.noop());
+        } else if (_.isDate(sValue)) {
+          _.set(mData, sKey, DateUtils.parse(sValue));
+        } else if (_.isNumber(sValue)) {
+          _.set(mData, sKey, _.toString(sValue));
         }
       },
     });
