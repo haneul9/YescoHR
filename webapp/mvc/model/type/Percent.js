e3be7458adb6a4ed2bbe5db14626787566dc9a13
유정우 <enmoei@gmail.com>
2021-12-29 09:35:22 +0900

    input용 Percent type 생성

---
+++ webapp/mvc/model/type/Percent.js
@@ -0,0 +1,84 @@
+sap.ui.define(
+  [
+    // prettier 방지용 주석
+    'sap/ui/model/FormatException',
+    'sap/ui/model/ParseException',
+    'sap/ui/model/SimpleType',
+    // 'sap/ui/yesco/common/AppUtils',
+  ],
+  (
+    // prettier 방지용 주석
+    FormatException,
+    ParseException,
+    SimpleType
+    // AppUtils
+  ) => {
+    'use strict';
+
+    return SimpleType.extend('sap.ui.yesco.mvc.model.type.Percent', {
+      formatValue(oValue, sTargetType) {
+        // AppUtils.debug(`sap/ui/yesco/mvc/model/type/Percent.formatValue(${oValue}, ${sTargetType})`);
+
+        switch (this.getPrimitiveType(sTargetType)) {
+          case 'string':
+          case 'any':
+            return this._toString(oValue, sTargetType);
+          case 'number':
+            return this._toNumber(oValue, sTargetType);
+          default:
+            throw new FormatException(`Don't know how to format Percent to ${sTargetType}`);
+        }
+      },
+
+      parseValue(oValue, sTargetType) {
+        // AppUtils.debug(`sap/ui/yesco/mvc/model/type/Percent.parseValue(${oValue}, ${sTargetType})`);
+
+        switch (this.getPrimitiveType(sTargetType)) {
+          case 'string':
+            return this._toString(oValue, sTargetType);
+          case 'number':
+          case 'any':
+            return this._toNumber(oValue, sTargetType);
+          default:
+            throw new ParseException(`Don't know how to parse Percent from ${sTargetType}`);
+        }
+      },
+
+      validateValue() {
+        return true;
+      },
+
+      _toString(oValue, sTargetType) {
+        if (!oValue) {
+          return '0';
+        }
+
+        if (typeof oValue === 'number' || oValue instanceof Number) {
+          return Math.min(100, Math.max(0, Math.abs(oValue)));
+        }
+
+        if (typeof oValue === 'string' || oValue instanceof String) {
+          return this._toString(Number(oValue));
+        }
+
+        throw new FormatException(`Don't know how to format Percent to ${sTargetType}`);
+      },
+
+      _toNumber(oValue, sTargetType) {
+        if (!oValue) {
+          return 0;
+        }
+
+        if (typeof oValue === 'number' || oValue instanceof Number) {
+          return Math.min(100, Math.max(0, Math.abs(oValue)));
+        }
+
+        if (typeof oValue === 'string' || oValue instanceof String) {
+          return this._toNumber(Number(oValue));
+        }
+
+        throw new ParseException(`Don't know how to parse Percent from ${sTargetType}`);
+      },
+    });
+  }
+);
