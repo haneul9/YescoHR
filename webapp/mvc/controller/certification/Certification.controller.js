1db50be2e9dd3688abcd62ce5d308b927069c2d3
JHU <HPJT0897@lottechem.com>
2022-01-04 10:23:11 +0900

    제증명신청 준비

---
+++ webapp/mvc/controller/certification/Certification.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define(
+  [
+    // prettier 방지용 주석
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/yesco/common/AppUtils',
+    'sap/ui/yesco/common/AttachFileAction',
+    'sap/ui/yesco/common/ComboEntry',
+    'sap/ui/yesco/common/FragmentEvent',
+    'sap/ui/yesco/common/TableUtils',
+    'sap/ui/yesco/common/TextUtils',
+    'sap/ui/yesco/common/odata/ServiceNames',
+    'sap/ui/yesco/common/exceptions/ODataReadError',
+    'sap/ui/yesco/mvc/controller/BaseController',
+    'sap/ui/yesco/mvc/model/type/Currency',
+  ],
+  function (JSONModel, AppUtils, AttachFileAction, ComboEntry, FragmentEvent, TableUtils, TextUtils, ServiceNames, ODataReadError, BaseController) {
+    'use strict';
+
+    return BaseController.extend('sap.ui.yesco.mvc.controller.certification.Certification', {});
+  }
+);
