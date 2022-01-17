5a0aae99da59eb20c08a9b4c4a23c2091d91f8d2
JHU <HPJT0897@lottechem.com>
2022-01-17 13:38:15 +0900

    학자금 수정

--- webapp/mvc/controller/studentFunds/StudentFundsDetail.controller.js
+++ webapp/mvc/controller/studentFunds/StudentFundsDetail.controller.js
@@ -518,6 +518,7 @@ sap.ui.define(
         oDetailModel.setProperty('/FormData/Appno', '');
         oDetailModel.setProperty('/FormData/ZappStatAl', '');
         this.settingsAttachTable();
+        this.onShcoolList();
       },
 
       // 임시저장
