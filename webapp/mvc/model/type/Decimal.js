df5ed1de2e3a80aee3aaea5fde9408cd652c41d9
성환희 <haneul9@gmail.com>
2022-02-10 09:58:08 +0900

    평가

---
+++ webapp/mvc/model/type/Decimal.js
@@ -0,0 +1,23 @@
+sap.ui.define(
+  [
+    // prettier 방지용 주석
+    'sap/ui/model/type/Float',
+  ],
+  (
+    // prettier 방지용 주석
+    Float
+  ) => {
+    'use strict';
+
+    /**
+     * Leading zero trim
+     */
+    return Float.extend('sap.ui.yesco.mvc.model.type.Decimal', {
+      formatValue() {
+        const dValue = Float.prototype.formatValue.apply(this, arguments);
+
+        return _.isEqual(_.toNumber(dValue), 0) ? '' : dValue;
+      },
+    });
+  }
+);
