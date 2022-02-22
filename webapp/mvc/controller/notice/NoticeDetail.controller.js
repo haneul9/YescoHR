d7ecbce50eeadd55a3fdba8b5e2b229cb57cd2e4
JHU <HPJT0897@lottechem.com>
2022-02-22 13:38:19 +0900

    공지사항 수정

--- webapp/mvc/controller/notice/NoticeDetail.controller.js
+++ webapp/mvc/controller/notice/NoticeDetail.controller.js
@@ -1,16 +1,13 @@
 /* eslint-disable no-useless-call */
 sap.ui.define(
   [
-    // prettier 방지용 주석
-    'sap/ui/model/json/JSONModel',
+    // prettier 방지주석
     'sap/ui/yesco/common/Appno',
     'sap/ui/yesco/common/AppUtils',
     'sap/ui/yesco/common/AttachFileAction',
     'sap/ui/yesco/common/odata/ServiceNames',
     'sap/ui/yesco/common/odata/Client',
     'sap/ui/yesco/control/MessageBox',
-    'sap/ui/yesco/common/exceptions/ODataReadError',
-    'sap/ui/yesco/common/exceptions/ODataCreateError',
     'sap/ui/yesco/common/exceptions/ODataDeleteError',
     'sap/ui/yesco/mvc/controller/BaseController',
     'sap/ui/richtexteditor/RichTextEditor',
@@ -18,15 +15,12 @@ sap.ui.define(
   ],
   (
     // prettier 방지용 주석
-    JSONModel,
     Appno,
     AppUtils,
     AttachFileAction,
     ServiceNames,
     Client,
     MessageBox,
-    ODataReadError,
-    ODataCreateError,
     ODataDeleteError,
     BaseController,
     RTE
@@ -82,7 +76,6 @@ sap.ui.define(
             const oSessionData = this.getSessionData();
 
             oDetailModel.setProperty('/MySelf', true);
-            oDetailModel.setProperty('/FormData', oSessionData);
             oDetailModel.setProperty('/FormData', {
               ApernTxt: `${oSessionData.Orgtx} ${oSessionData.Ename}`,
               Apern: oSessionData.Pernr,
@@ -117,9 +110,9 @@ sap.ui.define(
             oDetailModel.setProperty('/FormData', oTargetData);
           }
 
-          setTimeout(() => {
-            this.setTextEditor();
-          }, 100);
+          // setTimeout(() => {
+          //   this.setTextEditor();
+          // }, 100);
 
           this.settingsAttachTable();
         } catch (oError) {
@@ -346,7 +339,7 @@ sap.ui.define(
         const bStat = !!oDetailModel.getProperty('/MySelf') && !!oDetailModel.getProperty('/Hass');
         const oRichTextEditor = new RTE('myRTE', {
           editorType: sap.ui.richtexteditor.EditorType.TinyMCE4, // 'TinyMCE4',
-          // layoutData: new sap.m.FlexItemData({ growFactor: 1 }),
+          layoutData: new sap.m.FlexItemData({ growFactor: 1 }),
           width: '99.8%',
           height: '500px',
           customToolbar: bStat,
