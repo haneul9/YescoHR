91ff11ee6bd0a9eefe2624330059185ed480eeb4
Jiyoung Pyo <jiyoung.pyo@psnc.co.kr>
2022-03-23 16:58:02 +0900

    모바일 급여명세서

---
+++ webapp/mvc/controller/accountChange/mobile/AccountChange.controller.js
@@ -0,0 +1,243 @@
+sap.ui.define(
+  [
+    // prettier 방지용 주석
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/yesco/common/AppUtils',
+    'sap/ui/yesco/common/AttachFileAction',
+    'sap/ui/yesco/common/FragmentEvent',
+    'sap/ui/yesco/common/TableUtils',
+    'sap/ui/yesco/common/TextUtils',
+    'sap/ui/yesco/common/odata/Client',
+    'sap/ui/yesco/common/odata/ServiceNames',
+    'sap/ui/yesco/mvc/controller/BaseController',
+    'sap/ui/yesco/common/mobile/ListStatusPopover',
+    'sap/ui/yesco/mvc/model/type/Currency',
+    'sap/ui/yesco/mvc/model/type/Date', // DatePicker 에러 방지 import : Loading of data failed: Error: Date must be a JavaScript date object
+  ],
+  (
+    // prettier 방지용 주석
+    JSONModel,
+    AppUtils,
+    AttachFileAction,
+    FragmentEvent,
+    TableUtils,
+    TextUtils,
+    Client,
+    ServiceNames,
+    BaseController,
+    ListStatusPopover
+  ) => {
+    'use strict';
+
+    return BaseController.extend('sap.ui.yesco.mvc.controller.accountChange.mobile.AccountChange', {
+      AttachFileAction: AttachFileAction,
+      TableUtils: TableUtils,
+      TextUtils: TextUtils,
+      FragmentEvent: FragmentEvent,
+      ListStatusPopover: ListStatusPopover,
+
+      initializeModel() {
+        return {
+          busy: false,
+          Data: [],
+          MyAcc: {},
+          search: {
+            dateRange: '1w',
+            secondDate: moment().toDate(),
+            date: moment().subtract(7, 'day').toDate(),
+            dateBox: false,
+          },
+          listInfo: {
+            totalCount: 0
+          },
+        };
+      },
+
+      async onObjectMatched() {
+        const oListModel = this.getViewModel();
+
+        try {
+          oListModel.setProperty('/busy', true);
+
+          const mPernr = {};
+
+          if (this.isHass()) {
+            const sPernr = this.getAppointeeProperty('Pernr');
+
+            mPernr.Pernr = sPernr;
+          }
+
+          const mMyAccPayLoad = {
+            Menid: this.getCurrentMenuId(),
+            ...mPernr,
+          };
+          const oModel = this.getModel(ServiceNames.PAY);
+          // 나의 계좌정보
+          const [aMyAcc] = await Client.getEntitySet(oModel, 'CurrentAcctInfo', mMyAccPayLoad);
+
+          oListModel.setProperty('/MyAcc', aMyAcc);
+
+          const mSearch = oListModel.getProperty('/search');
+          const mPayLoad = {
+            Apbeg: moment(mSearch.date).hours(9).toDate(),
+            Apend: moment(mSearch.secondDate).hours(9).toDate(),
+            Menid: this.getCurrentMenuId(),
+            ...mPernr,
+          };
+          
+          const aList = await Client.getEntitySet(oModel, 'BankAccount', mPayLoad);
+
+          oListModel.setProperty('/listInfo/totalCount', _.size(aList));
+          oListModel.setProperty('/Data', aList);
+        } catch (oError) {
+          AppUtils.handleError(oError);
+        } finally {
+          oListModel.setProperty('/busy', false);
+        }
+      },
+
+      formatYear(sYearMon) {
+        return !sYearMon ? '' : `${sYearMon.slice(0, 4)}-${sYearMon.slice(4)}` + ` ` + this.getBundleText("LABEL_26015");
+      },
+
+      onClick() {
+        this.getRouter().navTo('mobile/accountChange-detail', { oDataKey: 'N' });
+      },
+
+      // override AttachFileCode
+      getApprovalType() {
+        return 'HR16';
+      },
+
+      async onSearch() {
+        const oListModel = this.getViewModel();
+
+        try {
+          oListModel.setProperty('/busy', true);
+
+          const mPernr = {};
+
+          if (this.isHass()) {
+            const sPernr = this.getAppointeeProperty('Pernr');
+
+            mPernr.Pernr = sPernr;
+          }
+
+          const mMyAccPayLoad = {
+            Menid: this.getCurrentMenuId(),
+            ...mPernr,
+          };
+
+          const oModel = this.getModel(ServiceNames.PAY);
+          // 나의 계좌정보
+          const [aMyAcc] = await Client.getEntitySet(oModel, 'CurrentAcctInfo', mMyAccPayLoad);
+
+          oListModel.setProperty('/MyAcc', aMyAcc);
+
+          const mSearch = oListModel.getProperty('/search');
+          const mPayLoad = {
+            Apbeg: moment(mSearch.date).hours(9).toDate(),
+            Apend: moment(mSearch.secondDate).hours(9).toDate(),
+            Menid: this.getCurrentMenuId(),
+            ...mPernr,
+          };
+
+          const aList = await Client.getEntitySet(oModel, 'BankAccount', mPayLoad);
+
+          oListModel.setProperty('/listInfo/totalCount', _.size(aList));
+          oListModel.setProperty('/Data', aList);
+        } catch (oError) {
+          AppUtils.handleError(oError);
+        } finally {
+          oListModel.setProperty('/busy', false);
+        }
+      },
+
+      // onSelectRow(oEvent) {
+      //   const vPath = oEvent.getParameter('rowBindingContext').getPath();
+      //   const oListModel = this.getViewModel();
+      //   const oRowData = oListModel.getProperty(vPath);
+
+      //   this.getRouter().navTo('accountChange-detail', { oDataKey: oRowData.Appno });
+      // },
+
+      onSelectRow(oEvent) {
+        const vPath = oEvent.getSource().getBindingContext().getPath();
+        const oRowData = this.getViewModel().getProperty(vPath);
+
+        if (isNaN(oRowData.Appno)) return;
+
+        this.getRouter().navTo('mobile/accountChange-detail', { oDataKey: _.trimStart(oRowData.Appno, '0') });
+      },
+
+      // 날짜선택
+      async onSearchRange() {
+          const oViewModel = this.getViewModel();
+
+          try {
+            oViewModel.setProperty('/busy', true);
+
+            await this.onSearch();
+          } catch (oError) {
+            AppUtils.handleError(oError);
+          } finally {
+            oViewModel.setProperty('/busy', false);
+          }
+      },
+
+      // 검색 날짜 선택
+      async onSearchList(oEvent) {
+          const oViewModel = this.getViewModel();
+
+          try {
+            const sKey = oEvent.getSource().getSelectedKey();
+            let dBegda = moment().toDate();
+            let dEndda = moment().toDate();
+            let bDateRangeBox = false;
+
+            oViewModel.setProperty('/busy', true);
+
+            switch (sKey) {
+              case '1w':
+                dEndda = moment().subtract(7, 'day').toDate();
+                bDateRangeBox = false;
+                break;
+              case '1m':
+                dEndda = moment().subtract(1, 'months').toDate();
+                bDateRangeBox = false;
+                break;
+              case '3m':
+                dEndda = moment().subtract(3, 'months').toDate();
+                bDateRangeBox = false;
+                break;
+              case '6m':
+                dEndda = moment().subtract(6, 'months').toDate();
+                bDateRangeBox = false;
+                break;
+              case '12m':
+                dEndda = moment().subtract(12, 'months').toDate();
+                bDateRangeBox = false;
+                break;
+              case '0':
+                bDateRangeBox = true;
+                break;
+            }
+
+            if (!bDateRangeBox) {
+              oViewModel.setProperty('/search/secondDate', dBegda);
+              oViewModel.setProperty('/search/date', dEndda);
+
+              await this.onSearch();
+            }
+
+            oViewModel.setProperty('/search/dateBox', bDateRangeBox);
+          } catch (oError) {
+            AppUtils.handleError(oError);
+          } finally {
+            oViewModel.setProperty('/busy', false);
+          }
+      },
+
+    });
+  }
+);
---
+++ webapp/mvc/controller/accountChange/mobile/AccountChangeDetail.controller.js
@@ -0,0 +1,330 @@
+/* eslint-disable no-useless-call */
+sap.ui.define(
+  [
+    // prettier 방지용 주석
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/yesco/control/MessageBox',
+    'sap/ui/yesco/common/Appno',
+    'sap/ui/yesco/common/AppUtils',
+    'sap/ui/yesco/common/AttachFileAction',
+    'sap/ui/yesco/common/ComboEntry',
+    'sap/ui/yesco/common/FragmentEvent',
+    'sap/ui/yesco/common/TextUtils',
+    'sap/ui/yesco/common/TableUtils',
+    'sap/ui/yesco/common/odata/Client',
+    'sap/ui/yesco/common/odata/ServiceNames',
+    'sap/ui/yesco/mvc/controller/BaseController',
+    'sap/ui/yesco/mvc/model/type/Date',
+    'sap/ui/yesco/mvc/model/type/Currency',
+  ],
+  (
+    // prettier 방지용 주석
+    JSONModel,
+    MessageBox,
+    Appno,
+    AppUtils,
+    AttachFileAction,
+    ComboEntry,
+    FragmentEvent,
+    TextUtils,
+    TableUtils,
+    Client,
+    ServiceNames,
+    BaseController
+  ) => {
+    'use strict';
+
+    return BaseController.extend('sap.ui.yesco.mvc.controller.accountChange.mobile.AccountChangeDetail', {
+      AttachFileAction: AttachFileAction,
+      TextUtils: TextUtils,
+      TableUtils: TableUtils,
+      FragmentEvent: FragmentEvent,
+
+      initializeModel() {
+        return {
+          Hass: this.isHass(),
+          FormData: {},
+          FieldLimit: {},
+          AccType: [{ Zcode: 'A', Ztext: this.getBundleText('LABEL_26014') }],
+          BankList: [],
+          listInfo: {
+            rowCount: 1,
+            totalCount: 0,
+            progressCount: 0,
+            applyCount: 0,
+            approveCount: 0,
+            rejectCount: 0,
+            completeCount: 0,
+          },
+          busy: false,
+        };
+      },
+
+      async onObjectMatched(oParameter) {
+        const sDataKey = oParameter.oDataKey;
+        const oDetailModel = this.getViewModel();
+        const oModel = this.getModel(ServiceNames.PAY);
+
+        oDetailModel.setData(this.initializeModel());
+        oDetailModel.setProperty('/busy', true);
+
+        try {
+          // Input Field Imited
+          oDetailModel.setProperty('/FieldLimit', _.assignIn(this.getEntityLimit(ServiceNames.PAY, 'BankAccount')));
+
+          // 변경은행
+          const aBankList = await Client.getEntitySet(oModel, 'BanklCodeList');
+
+          oDetailModel.setProperty('/BankList', new ComboEntry({ codeKey: 'Bankl', valueKey: 'Banka', aEntries: aBankList }));
+
+          if (sDataKey === 'N' || !sDataKey) {
+            const mSessionData = this.getSessionData();
+            const mAppointeeData = this.getAppointeeData();
+
+            const mMyAccPayLoad = {
+              Menid: this.getCurrentMenuId(),
+              Pernr: mAppointeeData.Pernr,
+            };
+            // 나의 계좌정보
+            const [mMyAcc] = await Client.getEntitySet(oModel, 'CurrentAcctInfo', mMyAccPayLoad);
+
+            oDetailModel.setProperty('/FormData', {
+              Pernr: mAppointeeData.Pernr,
+              Acctty: 'A',
+              Bankl: 'ALL',
+              Begym: moment().format('YYYYMM'),
+              PayYearMon: moment().format('YYYY-MM'),
+              BankaBef: _.get(mMyAcc, 'Banka'),
+              BanklBef: _.get(mMyAcc, 'Bankl'),
+              BanknBef: _.get(mMyAcc, 'Bankn'),
+            });
+
+            oDetailModel.setProperty('/ApplyInfo', {
+              Apename: mSessionData.Ename,
+              Aporgtx: `${mSessionData.Btrtx} / ${mSessionData.Orgtx}`,
+              Apjikgbtl: `${mSessionData.Zzjikgbt} / ${mSessionData.Zzjikcht}`,
+            });
+          } else {
+            const [oTargetData] = await Client.getEntitySet(oModel, 'BankAccount', {
+              Appno: sDataKey,
+            });
+
+            oTargetData.PayYearMon = `${oTargetData.Begym.slice(0, 4)}-${oTargetData.Begym.slice(4)}`;
+
+            oDetailModel.setProperty('/FormData', oTargetData);
+            oDetailModel.setProperty('/ApplyInfo', oTargetData);
+            oDetailModel.setProperty('/ApprovalDetails', oTargetData);
+          }
+
+          this.settingsAttachTable();
+        } catch (oError) {
+          this.debug(oError);
+          AppUtils.handleError(oError);
+        } finally {
+          oDetailModel.setProperty('/busy', false);
+        }
+      },
+
+      // override AttachFileCode
+      getApprovalType() {
+        return 'HR16';
+      },
+
+      getCurrentLocationText(oArguments) {
+        const sAction = oArguments.oDataKey === 'N' ? this.getBundleText('LABEL_04002') : this.getBundleText('LABEL_00165');
+
+        return sAction;
+      },
+
+      // 계좌구분 선택
+      onAccChange(oEvent) {
+        const oDetailModel = this.getViewModel();
+        const sKey = oEvent.getSource().getSelectedKey();
+
+        oDetailModel.setProperty('/FormData/Chkyn', '');
+        oDetailModel.setProperty('/FormData/Bankl', 'ALL');
+        oDetailModel.setProperty('/FormData/Bankn', '');
+      },
+
+      // 변경된 은행선택
+      onBankList() {
+        const oDetailModel = this.getViewModel();
+
+        oDetailModel.setProperty('/FormData/Chkyn', '');
+      },
+
+      // 계좌실명확인 Btn
+      async onAccNameCheck() {
+        if (this.checkError()) return;
+
+        const oDetailModel = this.getViewModel();
+        const mFormData = oDetailModel.getProperty('/FormData');
+        const oModel = this.getModel(ServiceNames.PAY);
+        const mPayLoad = {
+          Menid: this.getCurrentMenuId(),
+          Pernr: this.getAppointeeProperty('Pernr'),
+          Bankl: mFormData.Bankl,
+          Bankn: mFormData.Bankn,
+        };
+        // 실명확인
+        const aAccCheck = await Client.getEntitySet(oModel, 'CheckAccount', mPayLoad);
+
+        oDetailModel.setProperty('/FormData/Chkyn', aAccCheck[0].Chkyn);
+      },
+
+      // 지급년월
+      onDatePick(oEvent) {
+        const oDetailModel = this.getViewModel();
+        const dDateValue = oEvent.getSource().getDateValue();
+
+        oDetailModel.setProperty('/FormData/PayYearMon', moment(dDateValue).format('YYYY-MM'));
+        oDetailModel.setProperty('/FormData/Begym', moment(dDateValue).format('YYYYMM'));
+      },
+
+      // 변경된 은행계좌입력시
+      onAccChangeInput() {
+        const oDetailModel = this.getViewModel();
+
+        oDetailModel.setProperty('/FormData/Chkyn', '');
+      },
+
+      checkError(sType) {
+        const oDetailModel = this.getViewModel();
+        const mFormData = oDetailModel.getProperty('/FormData');
+
+        // 변경은행
+        if (mFormData.Bankl === 'ALL' || !mFormData.Bankl) {
+          MessageBox.alert(this.getBundleText('MSG_26004'));
+          return true;
+        }
+
+        // 변경계좌
+        if (!mFormData.Bankn) {
+          MessageBox.alert(this.getBundleText('MSG_26005'));
+          return true;
+        }
+
+        // 계좌실명확인
+        if (sType === 'C' && mFormData.Chkyn !== 'X') {
+          MessageBox.alert(this.getBundleText('MSG_26006'));
+          return true;
+        }
+
+        // 첨부파일
+        if (sType === 'C' && !AttachFileAction.getFileCount.call(this)) {
+          MessageBox.alert(this.getBundleText('MSG_00046'));
+          return true;
+        }
+
+        return false;
+      },
+
+      // 신청
+      onApplyBtn() {
+        if (this.checkError('C')) return;
+
+        // {신청}하시겠습니까?
+        MessageBox.confirm(this.getBundleText('MSG_00006', 'LABEL_00121'), {
+          // 신청, 취소
+          actions: [this.getBundleText('LABEL_00121'), this.getBundleText('LABEL_00118')],
+          onClose: async (vPress) => {
+            // 신청
+            if (!vPress || vPress !== this.getBundleText('LABEL_00121')) {
+              return;
+            }
+
+            try {
+              AppUtils.setAppBusy(true, this);
+
+              const oDetailModel = this.getViewModel();
+              const sAppno = oDetailModel.getProperty('/FormData/Appno');
+
+              if (!sAppno) {
+                const sAppno = await Appno.get.call(this);
+
+                oDetailModel.setProperty('/FormData/Appno', sAppno);
+                oDetailModel.setProperty('/FormData/Appdt', new Date());
+              }
+
+              const mFormData = oDetailModel.getProperty('/FormData');
+
+              // FileUpload
+              if (!!AttachFileAction.getFileCount.call(this)) {
+                await AttachFileAction.uploadFile.call(this, mFormData.Appno, this.getApprovalType());
+              }
+
+              const oModel = this.getModel(ServiceNames.PAY);
+              let oSendObject = {
+                ...mFormData,
+                Menid: this.getCurrentMenuId(),
+              };
+
+              await Client.create(oModel, 'BankAccount', oSendObject);
+
+              // {신청}되었습니다.
+              MessageBox.alert(this.getBundleText('MSG_00007', 'LABEL_00121'), {
+                onClose: () => {
+                  this.onNavBack();
+                },
+              });
+            } catch (oError) {
+              AppUtils.handleError(oError);
+            } finally {
+              AppUtils.setAppBusy(false, this);
+            }
+          },
+        });
+      },
+
+      // 삭제
+      onDeleteBtn() {
+        // {삭제}하시겠습니까?
+        MessageBox.confirm(this.getBundleText('MSG_00006', 'LABEL_00110'), {
+          // 삭제, 취소
+          actions: [this.getBundleText('LABEL_00110'), this.getBundleText('LABEL_00118')],
+          onClose: async (vPress) => {
+            // 삭제
+            if (!vPress || vPress !== this.getBundleText('LABEL_00110')) {
+              return;
+            }
+
+            AppUtils.setAppBusy(true, this);
+
+            try {
+              const oDetailModel = this.getViewModel();
+              const oModel = this.getModel(ServiceNames.PAY);
+
+              await Client.remove(oModel, 'BankAccount', { Appno: oDetailModel.getProperty('/FormData/Appno') });
+
+              // {삭제}되었습니다.
+              MessageBox.alert(this.getBundleText('MSG_00007', 'LABEL_00110'), {
+                onClose: () => {
+                  this.onNavBack();
+                },
+              });
+            } catch (oError) {
+              AppUtils.handleError(oError);
+            } finally {
+              AppUtils.setAppBusy(false, this);
+            }
+          },
+        });
+      },
+
+      // AttachFileTable Settings
+      settingsAttachTable() {
+        const oDetailModel = this.getViewModel();
+        const sStatus = oDetailModel.getProperty('/FormData/ZappStatAl');
+        const sAppno = oDetailModel.getProperty('/FormData/Appno') || '';
+
+        AttachFileAction.setAttachFile(this, {
+          Editable: !sStatus,
+          Type: this.getApprovalType(),
+          Appno: sAppno,
+          Max: 10,
+          FileTypes: ['jpg', 'pdf', 'doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx', 'bmp', 'txt', 'png'],
+        });
+      },
+    });
+  }
+);
