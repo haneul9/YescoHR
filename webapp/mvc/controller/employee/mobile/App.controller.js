b204ec41fb482a0a30139f786e54416e63904398
성환희 <haneul9@gmail.com>
2022-04-01 13:04:06 +0900

    사원프로파일

--- webapp/mvc/controller/employee/mobile/App.controller.js
+++ webapp/mvc/controller/employee/mobile/App.controller.js
@@ -53,7 +53,7 @@ sap.ui.define(
         9006: '3fr 3fr 2fr', // 자격
         '0183': '1fr 1fr 1fr', // 포상
         '0021': '1fr 1fr 1fr', // 가족
-        // EDU1: '2fr 3fr', // 교육
+        EDU1: '1fr 1fr', // 교육
         JOBC: '1fr 1fr', // 직무경력
         JOBL: '1fr 1fr', // 직무이력
       },
