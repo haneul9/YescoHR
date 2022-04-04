32d90e9cc9289106602a7df4aa38e63362beb23d
유정우 <enmoei@gmail.com>
2022-04-04 17:45:10 +0900

    PC 알림센터 popover 높이 수정

--- webapp/mvc/controller/app/NotificationPopoverHandler.js
+++ webapp/mvc/controller/app/NotificationPopoverHandler.js
@@ -48,7 +48,7 @@ sap.ui.define(
           listCount: 0,
           unreadCount: 0,
           showUnreadCount: false,
-          maxRows: this.bMobile ? Math.ceil((screen.availHeight - 143) / 69) : Math.floor(screen.availHeight / 69),
+          maxRows: this.bMobile ? Math.ceil((screen.availHeight - 143) / 69) : 5,
         };
       },
 
@@ -92,6 +92,7 @@ sap.ui.define(
 
         oNotificationModel.setProperty('/showUnreadCount', unreadCount > 0);
         oNotificationModel.setProperty('/unreadCount', `${unreadCount > 99 ? '+' : ''}${iUnreadCount}`);
+        oNotificationModel.setProperty('/maxRows', Math.min(Math.floor(screen.availHeight / 69), listCount));
         oNotificationModel.setProperty('/listCount', listCount);
         oNotificationModel.setProperty('/list', list);
       },
@@ -165,6 +166,7 @@ sap.ui.define(
 
         oNotificationModel.setProperty('/showUnreadCount', iUnreadCount > 0);
         oNotificationModel.setProperty('/unreadCount', `${iUnreadCount === 99 ? '+' : ''}${iUnreadCount}`);
+        oNotificationModel.setProperty('/maxRows', Math.min(Math.floor(screen.availHeight / 69), listCount + iPrevListCount));
         oNotificationModel.setProperty('/listCount', listCount + iPrevListCount);
         oNotificationModel.setProperty('/list', iPrevList);
 
