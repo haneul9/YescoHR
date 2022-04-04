fb77accfd73a93c89a0dece6e4f4d3a1cddac335
성환희 <haneul9@gmail.com>
2022-04-04 14:31:30 +0900

    급여명세서

--- webapp/mvc/controller/paystub/mobile/Detail.controller.js
+++ webapp/mvc/controller/paystub/mobile/Detail.controller.js
@@ -171,7 +171,7 @@ sap.ui.define(
           });
 
           if (mResult.Url && AppUtils.isPRD()) {
-            mResult.Url = `https://hrportal.yescoholdings.com:443/${_.chain(mResult.Url).split('/').drop(3).join('/').value()}`;
+            mResult.Url = `https://hrportal.yescoholdings.com:443/${_.chain(mResult.Url).split('/').drop(3).join('/').value()}?saml2=disabled`;
           }
 
           this.getViewModel().setProperty('/PdfUrl', mResult.Url);
