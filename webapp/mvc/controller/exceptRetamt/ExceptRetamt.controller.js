2357ea4c7759cc5edd4f7f0219aff2e04f27e3fe
Jiyoung Pyo <jiyoung.pyo@psnc.co.kr>
2022-04-29 17:57:21 +0900

    예상퇴직금조회

--- webapp/mvc/controller/exceptRetamt/ExceptRetamt.controller.js
+++ webapp/mvc/controller/exceptRetamt/ExceptRetamt.controller.js
@@ -33,7 +33,8 @@ sap.ui.define(
       async onObjectMatched() {
         const oViewModel = this.getViewModel();
 
-        if (AppUtils.isPRD() && !this.serviceAvailable()) return;
+        // if (AppUtils.isPRD() && !this.serviceAvailable()) return;
+        if (!this.serviceAvailable()) return;
 
         try {
           oViewModel.setProperty('/busy', true);
@@ -70,11 +71,18 @@ sap.ui.define(
       },
 
       serviceAvailable() {
-        const bOpen = moment().isAfter(moment('2022-04-29 09:00', 'YYYY-MM-DD HH:mm'));
+        const sWerks = this.getSessionProperty('Werks');
 
+        if(sWerks == "2000"){
+          const bOpen = moment().isAfter(moment('2022-04-29 09:00', 'YYYY-MM-DD HH:mm')); 
+          const sMessage = this.getBundleText('MSG_33005', 'LABEL_00110'); // 예상퇴직금 조회 서비스는 4/30일에 오픈할 예정이니 양해 부탁드립니다.
+        } else {
+          const bOpen = moment().isAfter(moment('2022-05-03 18:00', 'YYYY-MM-DD HH:mm'));
+          const sMessage = this.getBundleText('MSG_33006', 'LABEL_00110'); // 예상퇴직금 조회 서비스는 5/3에 오픈할 예정이니 양해 부탁드립니다.
+        }        
+        console.log(bOpen, sMessage)
         if (!bOpen)
-          // 예상퇴직금 조회 서비스는 금액 검증 후 4/29 09시에 정식 오픈할 예정이니 양해 부탁드립니다.
-          MessageBox.alert(this.getBundleText('MSG_33005', 'LABEL_00110'), {
+          MessageBox.alert(sMessage, {
             onClose: () => this.onNavBack(),
           });
 
