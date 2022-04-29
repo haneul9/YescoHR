94135c32775d8f925b734539476d5495d1430944
Jiyoung Pyo <jiyoung.pyo@psnc.co.kr>
2022-04-29 18:07:14 +0900

    .

--- webapp/mvc/controller/exceptRetamt/ExceptRetamt.controller.js
+++ webapp/mvc/controller/exceptRetamt/ExceptRetamt.controller.js
@@ -74,7 +74,7 @@ sap.ui.define(
         const sWerks = this.getSessionProperty('Werks');
 
         if(sWerks == "2000"){
-          const bOpen = moment().isAfter(moment('2022-04-29 09:00', 'YYYY-MM-DD HH:mm')); 
+          const bOpen = moment().isAfter(moment('2022-04-30 09:00', 'YYYY-MM-DD HH:mm')); 
           const sMessage = this.getBundleText('MSG_33005', 'LABEL_00110'); // 예상퇴직금 조회 서비스는 4/30일에 오픈할 예정이니 양해 부탁드립니다.
 
           if (!bOpen)
