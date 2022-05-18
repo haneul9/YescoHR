7e8f71981927c700e563206fc628e4111dd15b75
성환희 <haneul9@gmail.com>
2022-05-18 09:36:51 +0900

    선택적근로제

--- webapp/mvc/controller/flextime/Main.controller.js
+++ webapp/mvc/controller/flextime/Main.controller.js
@@ -31,27 +31,27 @@ sap.ui.define(
           summary: {
             rowCount: 1,
             list: [
-              { Todo1: moment().format('YYYYMM'), Todo2: '31', Todo3: '22', Todo4: '177', Todo5: '196', Todo6: '194', Todo7: '-2', Todo8: '48.50', Todo9: '계약근로시간 미달', Todo9Status: 'X', Todo10: moment('20220405').toDate() }, //
+              { Zyymm: moment().format('YYYYMM'), Caldays: '31', Wrkdays: '22', Bastim: '177', Ctrtim: '196', Daytim: '194', Gaptim: '-2', Wekavg: '48.50', Statxt: '계약근로시간 미달', Stacol: '1', Clsda: moment('20220405').toDate() }, //
             ],
           },
           details: {
             rowCount: 9,
             list: [
-              { Holiday: 'X', Todo1: moment('20220301').toDate(), Todo2: '화', Todo3: '', Todo4: '0900', Todo5: '1800', Todo6: '1.00', Todo7: '0.00', Todo8: '', Todo9: '8.00', Todo10: '8.00', Todo11: '1.00', Todo12: '' }, //
-              { Holiday: '', Todo1: moment('20220302').toDate(), Todo2: '수', Todo3: '', Todo4: '0900', Todo5: '1800', Todo6: '1.00', Todo7: '0.00', Todo8: '8.00', Todo9: '', Todo10: '16.00', Todo11: '1.00', Todo12: '' },
-              { Holiday: '', Todo1: moment('20220303').toDate(), Todo2: '목', Todo3: '연차', Todo4: '', Todo5: '', Todo6: '', Todo7: '', Todo8: '', Todo9: '8.00', Todo10: '24.00', Todo11: '', Todo12: '' },
-              { Holiday: '', Todo1: moment('20220304').toDate(), Todo2: '금', Todo3: '', Todo4: '1000', Todo5: '1500', Todo6: '1.00', Todo7: '0.00', Todo8: '4.50', Todo9: '', Todo10: '28.50', Todo11: '0.50', Todo12: '' },
-              { Holiday: 'X', Todo1: moment('20220305').toDate(), Todo2: '토', Todo3: '', Todo4: '', Todo5: '', Todo6: '', Todo7: '', Todo8: '', Todo9: '', Todo10: '28.50', Todo11: '', Todo12: '' },
-              { Holiday: 'X', Todo1: moment('20220306').toDate(), Todo2: '일', Todo3: '', Todo4: '', Todo5: '', Todo6: '', Todo7: '', Todo8: '', Todo9: '', Todo10: '28.50', Todo11: '', Todo12: '' },
-              { Holiday: '', Todo1: moment('20220307').toDate(), Todo2: '월', Todo3: '', Todo4: '0900', Todo5: '2130', Todo6: '0.50', Todo7: '0.00', Todo8: '12.00', Todo9: '', Todo10: '40.00', Todo11: '1.00', Todo12: '필수휴게시간 미달' },
-              { Holiday: '', Todo1: moment('20220308').toDate(), Todo2: '화', Todo3: '반차(오전)', Todo4: '1400', Todo5: '1800', Todo6: '0.00', Todo7: '0.00', Todo8: '4.00', Todo9: '4.00', Todo10: '48.00', Todo11: '0.00', Todo12: '시작시간 13시부터 가능' },
-              { Holiday: '', Todo1: moment('20220309').toDate(), Todo2: '수', Todo3: '반차(오후)', Todo4: '0900', Todo5: '1300', Todo6: '0.00', Todo7: '0.00', Todo8: '4.00', Todo9: '4.00', Todo10: '52.00', Todo11: '0.00', Todo12: '종료시간 14시까지 가능' },
+              { Holiday: 'X', Datum: moment('20220301').toDate(), Todo2: '화', Todo3: '', Beguz: 'PT09H00M00S', Enduz: '1800', Todo6: '1.00', Todo7: '0.00', Todo8: '', Todo9: '8.00', Todo10: '8.00', Todo11: '1.00', Todo12: '' }, //
+              { Holiday: '', Datum: moment('20220302').toDate(), Todo2: '수', Todo3: '', Beguz: '0900', Enduz: '1800', Todo6: '1.00', Todo7: '0.00', Todo8: '8.00', Todo9: '', Todo10: '16.00', Todo11: '1.00', Todo12: '' },
+              { Holiday: '', Datum: moment('20220303').toDate(), Todo2: '목', Todo3: '연차', Beguz: '', Enduz: '', Todo6: '', Todo7: '', Todo8: '', Todo9: '8.00', Todo10: '24.00', Todo11: '', Todo12: '' },
+              { Holiday: '', Datum: moment('20220304').toDate(), Todo2: '금', Todo3: '', Beguz: '1000', Enduz: '1500', Todo6: '1.00', Todo7: '0.00', Todo8: '4.50', Todo9: '', Todo10: '28.50', Todo11: '0.50', Todo12: '' },
+              { Holiday: 'X', Datum: moment('20220305').toDate(), Todo2: '토', Todo3: '', Beguz: '', Enduz: '', Todo6: '', Todo7: '', Todo8: '', Todo9: '', Todo10: '28.50', Todo11: '', Todo12: '' },
+              { Holiday: 'X', Datum: moment('20220306').toDate(), Todo2: '일', Todo3: '', Beguz: '', Enduz: '', Todo6: '', Todo7: '', Todo8: '', Todo9: '', Todo10: '28.50', Todo11: '', Todo12: '' },
+              { Holiday: '', Datum: moment('20220307').toDate(), Todo2: '월', Todo3: '', Beguz: '0900', Enduz: '2130', Todo6: '0.50', Todo7: '0.00', Todo8: '12.00', Todo9: '', Todo10: '40.00', Todo11: '1.00', Todo12: '필수휴게시간 미달' },
+              { Holiday: '', Datum: moment('20220308').toDate(), Todo2: '화', Todo3: '반차(오전)', Beguz: '1400', Enduz: '1800', Todo6: '0.00', Todo7: '0.00', Todo8: '4.00', Todo9: '4.00', Todo10: '48.00', Todo11: '0.00', Todo12: '시작시간 13시부터 가능' },
+              { Holiday: '', Datum: moment('20220309').toDate(), Todo2: '수', Todo3: '반차(오후)', Beguz: '0900', Enduz: '1300', Todo6: '0.00', Todo7: '0.00', Todo8: '4.00', Todo9: '4.00', Todo10: '52.00', Todo11: '0.00', Todo12: '종료시간 14시까지 가능' },
             ],
           },
           dialog: {
             work: { rowCount: 1, list: [] },
             legal: { rowCount: 1, list: [] },
-            extra: { rowCount: 1, list: [] },
+            extra: { rowCount: 2, list: [] },
           },
         };
       },
