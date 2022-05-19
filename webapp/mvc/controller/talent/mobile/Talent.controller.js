7baeafac47ebdeaed2926da09e41c046e84edbd0
성환희 <haneul9@gmail.com>
2022-05-19 17:00:44 +0900

    모바일-인재검색

--- webapp/mvc/controller/talent/mobile/Talent.controller.js
+++ webapp/mvc/controller/talent/mobile/Talent.controller.js
@@ -51,42 +51,42 @@ sap.ui.define(
           saved: {
             busy: false,
             Schtl: '',
-            selectedCondition: 'ALL',
+            selectedCondition: ['ALL'],
             entry: [],
           },
           search: {
             Command: 'AND',
-            Werks: '',
+            Werks: [],
             Freetx: '',
             Prcty: 'A',
             Jobgr: [],
             Zzjikgb: [],
             Zzjikch: [],
-            EeageFr: '',
-            EeageTo: '',
+            EeageFr: [],
+            EeageTo: [],
             Schcd: [],
             Major: [],
             Slabs: [],
             Cttyp: [],
-            Quali1: '',
-            Langlv1: '',
-            Quali2: '',
-            Langlv2: '',
-            Quali3: '',
-            Langlv3: '',
-            Gesch: '',
-            Stell1: '',
-            SyearFr1: '',
-            SyearTo1: '',
-            Stell2: '',
-            SyearFr2: '',
-            SyearTo2: '',
-            Stell3: '',
-            SyearFr3: '',
-            SyearTo3: '',
-            Stell4: '',
-            SyearFr4: '',
-            SyearTo4: '',
+            Quali1: [],
+            Langlv1: [],
+            Quali2: [],
+            Langlv2: [],
+            Quali3: [],
+            Langlv3: [],
+            Gesch: [],
+            Stell1: [],
+            SyearFr1: [],
+            SyearTo1: [],
+            Stell2: [],
+            SyearFr2: [],
+            SyearTo2: [],
+            Stell3: [],
+            SyearFr3: [],
+            SyearTo3: [],
+            Stell4: [],
+            SyearFr4: [],
+            SyearTo4: [],
           },
           result: {
             busy: false,
@@ -109,7 +109,7 @@ sap.ui.define(
           oViewModel.setProperty('/busy', true);
 
           await this.getEntrySearchCondition();
-          oViewModel.setProperty('/saved/selectedCondition', 'ALL');
+          oViewModel.setProperty('/saved/selectedCondition', ['ALL']);
 
           const oPAModel = this.getModel(ServiceNames.PA);
           const sPernr = this.getAppointeeProperty('Pernr');
@@ -126,7 +126,7 @@ sap.ui.define(
           const aNumberCodeEntry = ['D', 'M'];
 
           oViewModel.setProperty('/fieldLimit', _.assignIn(this.getEntityLimit(ServiceNames.PA, 'TalentSearch'), this.getEntityLimit(ServiceNames.PA, 'TalentSearchCondition')));
-          oViewModel.setProperty('/search/Werks', this.getAppointeeProperty('Werks'));
+          oViewModel.setProperty('/search/Werks', _.concat(this.getAppointeeProperty('Werks')));
           oViewModel.setProperty('/entry', {
             Werks: _.map(aEntryDataList[0], (o) => _.omit(o, '__metadata')),
             ..._.chain('ABCDEFGHIJKLM')
@@ -259,28 +259,32 @@ sap.ui.define(
       },
 
       onPairValue(oEvent) {
+        this.onMultiToSingleCombo(oEvent);
+
         const oViewModel = this.getViewModel();
         const oControl = oEvent.getSource();
-        const sValue = oControl.getSelectedKey();
+        const sValue = _.get(oControl.getSelectedKeys(), 0);
         const sTargetProp = oControl.data('target');
-        const sTargetValue = oViewModel.getProperty(`/search/${sTargetProp}`);
+        const sTargetValue = oViewModel.getProperty(`/search/${sTargetProp}/0`);
 
         if (_.isEmpty(sTargetValue) || sValue > sTargetValue) {
-          oViewModel.setProperty(`/search/${sTargetProp}`, oControl.getSelectedKey());
+          oViewModel.setProperty(`/search/${sTargetProp}`, _.concat(sValue));
         }
 
         if (_.isEmpty(sValue)) {
-          oViewModel.setProperty(`/search/${sTargetProp}`, '');
+          oViewModel.setProperty(`/search/${sTargetProp}`, []);
         }
 
-        oControl.getParent().getItems()[2].getBinding('items').filter(new Filter('Zcode', FilterOperator.GE, oControl.getSelectedKey()));
+        oControl.getParent().getItems()[2].getBinding('items').filter(new Filter('Zcode', FilterOperator.GE, sValue));
       },
 
-      async onChangeSearchCondition() {
+      async onChangeSearchCondition(oEvent) {
         const oViewModel = this.getViewModel();
 
         try {
-          const sSelectedCondition = oViewModel.getProperty('/saved/selectedCondition');
+          this.onMultiToSingleCombo(oEvent);
+
+          const sSelectedCondition = oViewModel.getProperty('/saved/selectedCondition/0');
 
           if (sSelectedCondition === 'ALL') return;
 
@@ -298,22 +302,26 @@ sap.ui.define(
       },
 
       onChangeQuali(oEvent) {
+        this.onMultiToSingleCombo(oEvent);
+
         const oViewModel = this.getViewModel();
         const sSeq = oEvent.getSource().data('seq');
 
-        oViewModel.setProperty(`/search/Langlv${sSeq}`, '');
+        oViewModel.setProperty(`/search/Langlv${sSeq}`, []);
       },
 
       onChangeStell(oEvent) {
+        this.onMultiToSingleCombo(oEvent);
+
         const oViewModel = this.getViewModel();
         const sSeq = oEvent.getSource().data('seq');
 
-        oViewModel.setProperty(`/search/SyearFr${sSeq}`, '');
-        oViewModel.setProperty(`/search/SyearTo${sSeq}`, '');
+        oViewModel.setProperty(`/search/SyearFr${sSeq}`, []);
+        oViewModel.setProperty(`/search/SyearTo${sSeq}`, []);
       },
 
       onPressDeleteSearchCondition() {
-        if (this.getViewModel().getProperty('/saved/selectedCondition') === 'ALL') return;
+        if (this.getViewModel().getProperty('/saved/selectedCondition/0') === 'ALL') return;
 
         MessageBox.confirm(this.getBundleText('MSG_35010'), {
           // 검색조건을 삭제하시겠습니까?
@@ -331,7 +339,7 @@ sap.ui.define(
         oViewModel.setProperty('/busy', true);
 
         try {
-          const sSelectedCondition = oViewModel.getProperty('/saved/selectedCondition');
+          const sSelectedCondition = oViewModel.getProperty('/saved/selectedCondition/0');
 
           await Client.remove(this.getModel(ServiceNames.PA), 'TalentSearchCondition', {
             Pernr: this.getAppointeeProperty('Pernr'),
@@ -378,7 +386,7 @@ sap.ui.define(
 
           await this.getEntrySearchCondition();
 
-          oViewModel.setProperty('/saved/selectedCondition', sConditionSubject);
+          oViewModel.setProperty('/saved/selectedCondition', _.concat(sConditionSubject));
 
           MessageBox.success(this.getBundleText('MSG_00007', 'LABEL_00103')); // {저장}되었습니다.
         } catch (oError) {
@@ -413,10 +421,27 @@ sap.ui.define(
               .set('Major', _.isEmpty(mSearchCondition.Major) ? [] : _.split(mSearchCondition.Major, '|'))
               .set('Slabs', _.isEmpty(mSearchCondition.Slabs) ? [] : _.split(mSearchCondition.Slabs, '|'))
               .set('Cttyp', _.isEmpty(mSearchCondition.Cttyp) ? [] : _.split(mSearchCondition.Cttyp, '|'))
-              .set('Stell1', mSearchCondition.Stell1 === '00000000' ? '' : mSearchCondition.Stell1)
-              .set('Stell2', mSearchCondition.Stell2 === '00000000' ? '' : mSearchCondition.Stell2)
-              .set('Stell3', mSearchCondition.Stell3 === '00000000' ? '' : mSearchCondition.Stell3)
-              .set('Stell4', mSearchCondition.Stell4 === '00000000' ? '' : mSearchCondition.Stell4)
+              .set('Stell1', mSearchCondition.Stell1 === '00000000' ? [] : _.concat(mSearchCondition.Stell1))
+              .set('Stell2', mSearchCondition.Stell2 === '00000000' ? [] : _.concat(mSearchCondition.Stell2))
+              .set('Stell3', mSearchCondition.Stell3 === '00000000' ? [] : _.concat(mSearchCondition.Stell3))
+              .set('Stell4', mSearchCondition.Stell4 === '00000000' ? [] : _.concat(mSearchCondition.Stell4))
+              .set('EeageFr', _.isEmpty(mSearchCondition.EeageFr) ? [] : _.concat(mSearchCondition.EeageFr))
+              .set('EeageTo', _.isEmpty(mSearchCondition.EeageTo) ? [] : _.concat(mSearchCondition.EeageTo))
+              .set('Quali1', _.isEmpty(mSearchCondition.Quali1) ? [] : _.concat(mSearchCondition.Quali1))
+              .set('Langlv1', _.isEmpty(mSearchCondition.Langlv1) ? [] : _.concat(mSearchCondition.Langlv1))
+              .set('Quali2', _.isEmpty(mSearchCondition.Quali2) ? [] : _.concat(mSearchCondition.Quali2))
+              .set('Langlv2', _.isEmpty(mSearchCondition.Langlv2) ? [] : _.concat(mSearchCondition.Langlv2))
+              .set('Quali3', _.isEmpty(mSearchCondition.Quali3) ? [] : _.concat(mSearchCondition.Quali3))
+              .set('Langlv3', _.isEmpty(mSearchCondition.Langlv3) ? [] : _.concat(mSearchCondition.Langlv3))
+              .set('Gesch', _.isEmpty(mSearchCondition.Gesch) ? [] : _.concat(mSearchCondition.Gesch))
+              .set('SyearFr1', _.isEmpty(mSearchCondition.SyearFr1) ? [] : _.concat(mSearchCondition.SyearFr1))
+              .set('SyearTo1', _.isEmpty(mSearchCondition.SyearTo1) ? [] : _.concat(mSearchCondition.SyearTo1))
+              .set('SyearFr2', _.isEmpty(mSearchCondition.SyearFr2) ? [] : _.concat(mSearchCondition.SyearFr2))
+              .set('SyearTo2', _.isEmpty(mSearchCondition.SyearTo2) ? [] : _.concat(mSearchCondition.SyearTo2))
+              .set('SyearFr3', _.isEmpty(mSearchCondition.SyearFr3) ? [] : _.concat(mSearchCondition.SyearFr3))
+              .set('SyearTo3', _.isEmpty(mSearchCondition.SyearTo3) ? [] : _.concat(mSearchCondition.SyearTo3))
+              .set('SyearFr4', _.isEmpty(mSearchCondition.SyearFr4) ? [] : _.concat(mSearchCondition.SyearFr4))
+              .set('SyearTo4', _.isEmpty(mSearchCondition.SyearTo4) ? [] : _.concat(mSearchCondition.SyearTo4))
               .value(),
           });
         } catch (oError) {
@@ -430,7 +455,7 @@ sap.ui.define(
         oViewModel.setProperty('/saved/busy', true);
 
         try {
-          const mSearch = oViewModel.getProperty('/search');
+          const mSearch = this.getSearchConditions();
           const mSchtl = oViewModel.getProperty('/saved/Schtl');
 
           await Client.create(this.getModel(ServiceNames.PA), 'TalentSearchCondition', {
@@ -474,8 +499,18 @@ sap.ui.define(
         const oViewModel = this.getViewModel();
 
         try {
-          const mSearch = _.cloneDeep(oViewModel.getProperty('/search'));
-          _.chain(mSearch).set('Freetx', _.chain(mSearch.Freetx).replace(/ /g, '').replace(/[,]/g, '/').value()).set('Cttyp', _.compact(mSearch.Cttyp)).set('Jobgr', _.compact(mSearch.Jobgr)).set('Major', _.compact(mSearch.Major)).set('Schcd', _.compact(mSearch.Schcd)).set('Slabs', _.compact(mSearch.Slabs)).set('Zzjikch', _.compact(mSearch.Zzjikch)).set('Zzjikgb', _.compact(mSearch.Zzjikgb)).commit();
+          const mSearch = this.getSearchConditions();
+
+          _.chain(mSearch)
+            .set('Freetx', _.chain(mSearch.Freetx).replace(/ /g, '').replace(/[,]/g, '/').value()) //
+            .set('Cttyp', _.compact(mSearch.Cttyp))
+            .set('Jobgr', _.compact(mSearch.Jobgr))
+            .set('Major', _.compact(mSearch.Major))
+            .set('Schcd', _.compact(mSearch.Schcd))
+            .set('Slabs', _.compact(mSearch.Slabs))
+            .set('Zzjikch', _.compact(mSearch.Zzjikch))
+            .set('Zzjikgb', _.compact(mSearch.Zzjikgb))
+            .commit();
 
           const mFilters = mSearch.Prcty === 'A' ? _.pick(mSearch, ['Freetx', 'Command', 'Prcty', 'Werks']) : _.omit(mSearch, 'Freetx', 'Command');
           const aSearchResults = await Client.getEntitySet(this.getModel(ServiceNames.PA), 'TalentSearch', { Pernr: this.getAppointeeProperty('Pernr'), ..._.omitBy(mFilters, _.isEmpty) });
@@ -493,13 +528,12 @@ sap.ui.define(
       },
 
       validSearchConditions() {
-        const oViewModel = this.getViewModel();
-        const mSearch = oViewModel.getProperty('/search');
+        const mSearch = this.getSearchConditions();
 
         if (mSearch.Prcty === 'A') {
           if (_.isEmpty(mSearch.Freetx)) throw new UI5Error({ code: 'A', message: this.getBundleText('MSG_35006') }); // 검색어를 입력하여 주십시오.
         } else {
-          if (_.chain(mSearch).omit(['Prcty', 'Werks']).omitBy(_.isEmpty).isEmpty().value()) throw new UI5Error({ code: 'A', message: this.getBundleText('MSG_35007') }); // 검색조건을 입력하여 주십시오.
+          if (_.chain(mSearch).omit(['Prcty', 'Werks', 'Command']).omitBy(_.isEmpty).isEmpty().value()) throw new UI5Error({ code: 'A', message: this.getBundleText('MSG_35007') }); // 검색조건을 입력하여 주십시오.
           if (_.toNumber(mSearch.EeageFr) > _.toNumber(mSearch.EeageTo)) throw new UI5Error({ code: 'A', message: this.getBundleText('MSG_35002') }); // 나이 입력값의 최소값이 최대값보다 큽니다.
           if (!_.isEmpty(mSearch.Quali1) && _.isEmpty(mSearch.Langlv1)) throw new UI5Error({ code: 'A', message: this.getBundleText('MSG_35003', 'LABEL_35009', '1') }); // {외국어}{1} 값을 입력하여 주십시오.
           if (!_.isEmpty(mSearch.Quali2) && _.isEmpty(mSearch.Langlv2)) throw new UI5Error({ code: 'A', message: this.getBundleText('MSG_35003', 'LABEL_35009', '2') }); // {외국어}{2} 값을 입력하여 주십시오.
@@ -567,6 +601,36 @@ sap.ui.define(
         });
       },
 
+      getSearchConditions() {
+        const mSearchConditions = this.getViewModel().getProperty('/search');
+
+        return _.chain(mSearchConditions)
+          .cloneDeep()
+          .set('Werks', _.get(mSearchConditions, ['Werks', 0]))
+          .set('EeageFr', _.get(mSearchConditions, ['EeageFr', 0]))
+          .set('EeageTo', _.get(mSearchConditions, ['EeageTo', 0]))
+          .set('Quali1', _.get(mSearchConditions, ['Quali1', 0]))
+          .set('Langlv1', _.get(mSearchConditions, ['Langlv1', 0]))
+          .set('Quali2', _.get(mSearchConditions, ['Quali2', 0]))
+          .set('Langlv2', _.get(mSearchConditions, ['Langlv2', 0]))
+          .set('Quali3', _.get(mSearchConditions, ['Quali3', 0]))
+          .set('Langlv3', _.get(mSearchConditions, ['Langlv3', 0]))
+          .set('Gesch', _.get(mSearchConditions, ['Gesch', 0]))
+          .set('Stell1', _.get(mSearchConditions, ['Stell1', 0]))
+          .set('SyearFr1', _.get(mSearchConditions, ['SyearFr1', 0]))
+          .set('SyearTo1', _.get(mSearchConditions, ['SyearTo1', 0]))
+          .set('Stell2', _.get(mSearchConditions, ['Stell2', 0]))
+          .set('SyearFr2', _.get(mSearchConditions, ['SyearFr2', 0]))
+          .set('SyearTo2', _.get(mSearchConditions, ['SyearTo2', 0]))
+          .set('Stell3', _.get(mSearchConditions, ['Stell3', 0]))
+          .set('SyearFr3', _.get(mSearchConditions, ['SyearFr3', 0]))
+          .set('SyearTo3', _.get(mSearchConditions, ['SyearTo3', 0]))
+          .set('Stell4', _.get(mSearchConditions, ['Stell4', 0]))
+          .set('SyearFr4', _.get(mSearchConditions, ['SyearFr4', 0]))
+          .set('SyearTo4', _.get(mSearchConditions, ['SyearTo4', 0]))
+          .value();
+      },
+
       onPressPic(oEvent) {
         const mRowData = oEvent.getSource().getParent().getParent().getParent().getBindingContext().getObject();
 
@@ -597,7 +661,7 @@ sap.ui.define(
         const oViewModel = this.getViewModel();
         const mSearch = oViewModel.getProperty('/search');
 
-        oViewModel.setProperty('/saved/selectedCondition', 'ALL');
+        oViewModel.setProperty('/saved/selectedCondition', ['ALL']);
         oViewModel.setProperty('/saved/Schtl', '');
         oViewModel.setProperty(
           '/search',
@@ -605,31 +669,31 @@ sap.ui.define(
             .set('Jobgr', [])
             .set('Zzjikgb', [])
             .set('Zzjikch', [])
-            .set('EeageFr', '')
-            .set('EeageTo', '')
+            .set('EeageFr', [])
+            .set('EeageTo', [])
             .set('Schcd', [])
             .set('Major', [])
             .set('Slabs', [])
             .set('Cttyp', [])
-            .set('Quali1', '')
-            .set('Langlv1', '')
-            .set('Quali2', '')
-            .set('Langlv2', '')
-            .set('Quali3', '')
-            .set('Langlv3', '')
-            .set('Gesch', '')
-            .set('Stell1', '')
-            .set('SyearFr1', '')
-            .set('SyearTo1', '')
-            .set('Stell2', '')
-            .set('SyearFr2', '')
-            .set('SyearTo2', '')
-            .set('Stell3', '')
-            .set('SyearFr3', '')
-            .set('SyearTo3', '')
-            .set('Stell4', '')
-            .set('SyearFr4', '')
-            .set('SyearTo4', '')
+            .set('Quali1', [])
+            .set('Langlv1', [])
+            .set('Quali2', [])
+            .set('Langlv2', [])
+            .set('Quali3', [])
+            .set('Langlv3', [])
+            .set('Gesch', [])
+            .set('Stell1', [])
+            .set('SyearFr1', [])
+            .set('SyearTo1', [])
+            .set('Stell2', [])
+            .set('SyearFr2', [])
+            .set('SyearTo2', [])
+            .set('Stell3', [])
+            .set('SyearFr3', [])
+            .set('SyearTo3', [])
+            .set('Stell4', [])
+            .set('SyearFr4', [])
+            .set('SyearTo4', [])
             .value()
         );
       },
