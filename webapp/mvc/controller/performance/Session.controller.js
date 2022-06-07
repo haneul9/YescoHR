f971db1a4b6a71574ea62d64de3352d1098d980a
Jiyoung Pyo <jiyoung.pyo@psnc.co.kr>
2022-06-07 13:23:18 +0900

    전사세션 수정

--- webapp/mvc/controller/performance/Session.controller.js
+++ webapp/mvc/controller/performance/Session.controller.js
@@ -64,7 +64,7 @@ sap.ui.define(
             item: [],
           },
           Werks: '',
-          isChange: '',
+          isChange: false,
         };
       },
 
@@ -89,7 +89,7 @@ sap.ui.define(
           const sSessionWerks = this.getSessionProperty('Werks');
 
           oViewModel.setProperty('/Werks', sSessionWerks);
-          oViewModel.setProperty('/isChange', sSessionWerks === '1000' || sSessionWerks === '5000' ? true : false);
+          oViewModel.setProperty('/isChange', sSessionWerks === '1000' || sSessionWerks === '5000');
 
           const oModel = this.getModel(ServiceNames.APPRAISAL);
           const [aGrades, mDetailData, aDialogGrade] = await Promise.all([
