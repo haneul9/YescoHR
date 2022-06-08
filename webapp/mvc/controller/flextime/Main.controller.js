77c4457979f7e9ae27ee862445590c8be7591792
Jiyoung Pyo <jiyoung.pyo@psnc.co.kr>
2022-06-08 15:27:23 +0900

    선택적근로제 수정

--- webapp/mvc/controller/flextime/Main.controller.js
+++ webapp/mvc/controller/flextime/Main.controller.js
@@ -142,9 +142,9 @@ sap.ui.define(
             Pernr: this.getAppointeeProperty('Pernr'),
             Zyymm: sYearMonth,
           });
-console.log(aResults)
+
           // 현재일 > 마감일자인 경우 조회모드로 변경
-          if(moment().format('YYYYMMDD') > moment(aResults.Clsda).format('YYYYMMDD')){
+          if(moment().format('YYYYMMDD') > moment(aResults[0].Clsda).format('YYYYMMDD')){
             oViewModel.setProperty('/isMss', true);
           }
 
